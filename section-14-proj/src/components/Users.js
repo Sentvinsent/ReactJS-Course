8d3df39d9cefda78ff7b1c2f810c0a2a1491fcfe
Sentvinsent <68552615+Sentvinsent@users.noreply.github.com>
2023-06-16 19:58:19 +0200

    Section 14 (completed)
    Class-based components

---
+++ section-14-proj/src/components/Users.js
@@ -0,0 +1,68 @@
+import { useState, Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+class Users extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showUser: true
+    }
+  }
+
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return {
+        showUser: !curState.showUser
+      }
+    })
+  };
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUser ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUser && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;

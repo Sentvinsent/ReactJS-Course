feeeac8cca090489071b91e8acfb13a7c983a90b
Aleks <68552615+Sentvinsent@users.noreply.github.com>
2024-04-15 20:01:57 +0200

    Section 25 (in progress)
    Error handling, server actions, form submissions with custom image picker.

--- section-25-proj-2/app/meals/page.js
+++ section-25-proj-2/app/meals/page.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function Meals() {
+async function LoadMeals() {
   const meals = await getMeals();
-  
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function Meals() {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +24,9 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <LoadMeals />
+        </Suspense>
       </main>
     </>
   );
---
+++ section-25-proj-2/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>NOT FOUND</h1>
+      <p>Can't find requested page.</p>
+    </main>
+  );
+}

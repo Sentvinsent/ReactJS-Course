7af1476641fa96f856d353016f618b65ed6f4d46
Aleks <68552615+Sentvinsent@users.noreply.github.com>
2024-05-20 20:28:27 +0200

    Section 28 (completed)
    Replacing Redux with React Hooks: context API, custom store using custom hook.

---
+++ section-28-proj/src/components/Products/ProductItem.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+// import { useDispatch } from 'react-redux';
+
+// import { ProductContext } from "../../context/products-context";
+import { useStore } from "../../hooks-store/store";
+import Card from "../UI/Card";
+import "./ProductItem.css";
+// import { toggleFav } from '../../store/actions/products';
+
+const ProductItem = React.Children.memo((props) => {
+  // const dispatch = useDispatch();
+  // const toggleFav = useContext(ProductContext).toggleFav;
+  const dispatch = useStore(false)[1];
+
+  const toggleFavHandler = () => {
+    // toggleFav(props.id)
+    // dispatch(toggleFav(props.id));
+    dispatch("TOGGLE_FAV", props.id);
+  };
+
+  return (
+    <Card style={{ marginBottom: "1rem" }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? "button-outline" : ""}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? "Un-Favorite" : "Favorite"}
+        </button>
+      </div>
+    </Card>
+  );
+});
+
+export default ProductItem;
